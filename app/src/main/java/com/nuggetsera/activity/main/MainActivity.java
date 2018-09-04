f005332ccf547190ad516ae85ff30e6bc07c7ea2
zhangzz <95785817@qq.com>
2018-09-04 15:54:46 +0800

    提交升级模块以及弹框

--- app/src/main/java/com/nuggetsera/activity/main/MainActivity.java
+++ app/src/main/java/com/nuggetsera/activity/main/MainActivity.java
@@ -1,10 +1,15 @@
 package com.nuggetsera.activity.main;
 
+import android.Manifest;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.dialoglibrary.dilaog.AppDialog;
+import com.example.updateapplibrary.AppUpdater;
 import com.nuggetsera.R;
 import com.nuggetsera.activity.invitefriend.InviteFriendActivity;
 import com.nuggetsera.activity.my.MyActivity;
@@ -45,6 +50,7 @@ public class MainActivity
     TextView mTvGoldValue;
 
     private MainPresenter mMainPresenter;
+    private String mUrl = "https://raw.githubusercontent.com/jenly1314/AppUpdater/master/app/release/app-release.apk";
 
     @Override
     public int getLayoutResId() {
@@ -70,7 +76,6 @@ public class MainActivity
         switch (view.getId()) {
             case R.id.tv_user_name:     //我的
                 goTo(MyActivity.class);
-
                 break;
             case R.id.tv_announcement:  //公告
 
@@ -79,7 +84,7 @@ public class MainActivity
                 goTo(InviteFriendActivity.class);
                 break;
             case R.id.iv_obtain_force:   //获取原力
-
+                updateMethod();
                 break;
             case R.id.iv_gold_act:       //紫金活动
 
@@ -94,5 +99,36 @@ public class MainActivity
 
                 break;
         }
+    }
+
+    private void updateMethod() {
+        View view = LayoutInflater.from(this).inflate(R.layout.dialog_custom, null);
+
+        TextView tvTitle = view.findViewById(R.id.tvTitle);
+        tvTitle.setText("自定义弹框升级");
+        TextView tvContent = view.findViewById(R.id.tvContent);
+        tvContent.setText("1、新增某某功能、\n2、修改某某问题、\n3、优化某某BUG、");
+
+        Button btnCancel = view.findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AppDialog.INSTANCE.dismissDialog();
+            }
+        });
+        Button btnOK = view.findViewById(R.id.btnOK);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AppUpdater.Builder()
+                        .serUrl(mUrl)
+                        .setFilename("AppUpdater.apk")
+                        .build(MainActivity.this)
+                        .start();
+                AppDialog.INSTANCE.dismissDialog();
+            }
+        });
+
+        AppDialog.INSTANCE.showDialog(this, view);
     }
 }
